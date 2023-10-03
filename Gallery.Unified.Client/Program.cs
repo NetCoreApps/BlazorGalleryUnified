cf7f796bedeaca7cc8be29ad5a06c17cf7083035
Darren Reid <darren@reidmail.org>
2023-10-03 14:25:20 +1100

    Add license

--- Gallery.Unified.Client/Program.cs
+++ Gallery.Unified.Client/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor.Extensions.Logging;
 
-Licensing.RegisterLicense("OSS BSD-3-Clause 2023 https://github.com/NetCoreApps/BlazorGallery WZ4hSuvzmoeRsZSygYGa3b7y1F+ohVZhYvHsfbRBw3hr0Jhyk/xSrPOl86g8St+H9zll7ehDjG5D5176JvP9baU3zIoZKek3+RvDbr+Th/COMaXrnByIA/pBjIR7aApF6l8tLXA4qV05rEE7wNxla74QGCupdH5NU2r2algvLTU=");
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddLogging(c => c
     .AddBrowserConsole()
--- Gallery.Unified/Program.cs
+++ Gallery.Unified/Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using MyApp;
 using MyApp.Client;
 
+Licensing.RegisterLicense("OSS BSD-3-Clause 2023 https://github.com/NetCoreApps/BlazorGallery WZ4hSuvzmoeRsZSygYGa3b7y1F+ohVZhYvHsfbRBw3hr0Jhyk/xSrPOl86g8St+H9zll7ehDjG5D5176JvP9baU3zIoZKek3+RvDbr+Th/COMaXrnByIA/pBjIR7aApF6l8tLXA4qV05rEE7wNxla74QGCupdH5NU2r2algvLTU=");
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
